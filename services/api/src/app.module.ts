cba75a0c1f3977e4234b1af236ec6d190863f695
Angela Davis <angeladavis2024@u.northwestern.edu>
2026-04-06 09:15:02 -0400

    test

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
-import { HealthController } from './health.controller';
-import { PrismaService } from '@tracelite/db';
 import { AuthModule } from './auth/auth.module';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { ApiKeysModule } from './api-keys/api-keys.module';
 import { TrackingModule } from './tracking/tracking.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { QueueModule } from './queue/queue.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  controllers: [HealthController],
-  providers: [PrismaService],
-  imports: [AuthModule, OrganizationsModule, ApiKeysModule, TrackingModule, AnalyticsModule, QueueModule],
+  imports: [DatabaseModule, AuthModule, OrganizationsModule, ApiKeysModule, TrackingModule, AnalyticsModule, QueueModule],
 })
-export class AppModule {}
+export class AppModule { }
--- services/api/src/auth/auth.service.ts
+++ services/api/src/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
+import { SignupDto } from './dto/signup.dto';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
+import { PrismaService } from '@tracelite/db';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService
+    ) { }
+
+    async signup(signupDto: SignupDto): Promise<{ accessToken: string }> {
+        const email = signupDto.email;
+        const doesEmailAlreadyExist = await this.prisma.user.findUnique({ where: { email } });
+        if (doesEmailAlreadyExist) {
+            throw new Error('Email already exists');
+        }
+        const password = await bcrypt.hash(signupDto.password, 10);
+        const user = await this.prisma.user.create({
+            data: {
+                email,
+                password
+            }
+        });
+        const token = this.jwtService.sign({ sub: user.id, email: user.email });
+        return { accessToken: token };
+    }
+
+    async login(loginDto: LoginDto): Promise<{ accessToken: string }> {
+        const { email, password } = loginDto;
+        const user = await this.prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            throw new Error('Email does not exist');
+        }
+        const doesPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!doesPasswordMatch) {
+            throw new Error('Incorrect password');
+        }
+        const token = this.jwtService.sign({ sub: user.id, email: user.email });
+        return { accessToken: token };
+    }
+}
